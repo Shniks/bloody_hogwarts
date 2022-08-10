bf40d5e8931e7017baaf00cab8996d52fa6d90f2
Nikhil Shahi <shniks@gmail.com>
2022-08-10 13:31:42 -0700

    Created course model test to count number of students.

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -11,6 +11,29 @@ RSpec.describe Course, type: :model do
     it {should have_many(:students).through(:student_courses)}
   end
 
+  it 'can count number of students' do
+    student_1 = Student.create!(name: "Harry Potter", age: 15, house: "Slitherin")
+    student_2 = Student.create!(name: "Paulie Paul", age: 14, house: "Bangin")
+    student_3 = Student.create!(name: "Frodo Baggins", age: 14, house: "Mordor")
+    student_4 = Student.create!(name: "Gandalf", age: 14, house: "Moria")
+
+    course_1 = Course.create!(name: "Wizardry")
+    course_2 = Course.create!(name: "Herbology")
+    course_3 = Course.create!(name: "Defense against the Dark Arts")
+
+    student_1.courses << course_1
+    student_2.courses << course_1
+    student_2.courses << course_2
+    student_3.courses << course_2
+    student_3.courses << course_3
+    student_4.courses << course_1
+    student_4.courses << course_3
+
+    expect(course_1.student_count).to eq(3)
+    expect(course_2.student_count).to eq(2)
+    expect(course_3.student_count).to eq(2)
+  end
+
   it 'can arrange courses alphabetically' do
     student_1 = Student.create!(name: "Harry Potter", age: 15, house: "Slitherin")
     student_2 = Student.create!(name: "Paulie Paul", age: 14, house: "Bangin")

e7d797f445ef3e2f9d0e0c6c045715c09e9398bd
Nikhil Shahi <shniks@gmail.com>
2022-08-10 14:02:05 -0700

    Updated feature test with assertions to check if the students names are sorted alphabetically.

--- spec/features/courses/user_can_see_student_details_on_courses_index_spec.rb
+++ spec/features/courses/user_can_see_student_details_on_courses_index_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe 'When I visit the courses index' do
     student_4.courses << course_3
 
     visit '/courses'
-  
+
     (1..3).each do |i|
       within ".course_#{i}" do
         course = Course.alphabetical[i-1]
@@ -54,6 +54,40 @@ RSpec.describe 'When I visit the courses index' do
     expect(course_3.name).to appear_before(course_1.name)
     expect(course_3.name).to appear_before(course_2.name)
     expect(course_2.name).to appear_before(course_1.name)
+  end
+
+  it 'The names of the students for each course are listed in alphabetical order' do
+
+    student_1 = Student.create!(name: "Harry Potter", age: 15, house: "Slitherin")
+    student_2 = Student.create!(name: "Paulie Paul", age: 14, house: "Bangin")
+    student_3 = Student.create!(name: "Frodo Baggins", age: 14, house: "Mordor")
+    student_4 = Student.create!(name: "Gandalf", age: 14, house: "Moria")
+
+    course_1 = Course.create!(name: "Wizardry")
+    course_2 = Course.create!(name: "Herbology")
+    course_3 = Course.create!(name: "Defense against the Dark Arts")
+
+    student_1.courses << course_1
+    student_2.courses << course_1
+    student_2.courses << course_2
+    student_3.courses << course_2
+    student_3.courses << course_3
+    student_4.courses << course_1
+    student_4.courses << course_3
+
+    visit '/courses'
+
+    within ".course_1" do
+      course = Course.alphabetical[0]
+      expect(page).to have_content("#{course.name}: #{course.student_count}")
+      expect(page).to have_content("Frodo Baggins - Gandalf")
+    end
+
+    within ".course_2" do
+      course = Course.alphabetical[1]
+      expect(page).to have_content("#{course.name}: #{course.student_count}")
+      expect(page).to have_content("Frodo Baggins - Paulie Paul")
+    end
   end
 
 end

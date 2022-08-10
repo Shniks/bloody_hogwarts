d4017d444762b355af7cd2801643a062c5900687
Nikhil Shahi <shniks@gmail.com>
2022-08-10 12:21:14 -0700

    Created feature test to arrange all courses alphabetically.

--- spec/features/courses/user_can_see_student_details_on_courses_index_spec.rb
+++ spec/features/courses/user_can_see_student_details_on_courses_index_spec.rb
@@ -22,4 +22,31 @@ RSpec.describe 'When I visit the courses index' do
     expect(page).to have_content("Defense against the Dark Arts: 3")
     expect(page).to have_content("Herbology: 2")
   end
+
+  it 'The names of the courses are listed in alphabetical order' do
+
+    student_1 = Student.create!(name: "Harry Potter", age: 15, house: "Slitherin")
+    student_2 = Student.create!(name: "Paulie Paul", age: 14, house: "Bangin")
+    student_3 = Student.create!(name: "Frodo Baggins", age: 14, house: "Mordor")
+    student_4 = Student.create!(name: "Gandalf", age: 14, house: "Moria")
+
+    course_1 = Course.create!(name: "Wizardry")
+    course_2 = Course.create!(name: "Herbology")
+    course_3 = Course.create!(name: "Defense against the Dark Arts")
+
+    StudentCourse.create!(student_id: student_1.id, course_id: course_1.id)
+    StudentCourse.create!(student_id: student_2.id, course_id: course_1.id)
+    StudentCourse.create!(student_id: student_4.id, course_id: course_1.id)
+    StudentCourse.create!(student_id: student_2.id, course_id: course_2.id)
+    StudentCourse.create!(student_id: student_3.id, course_id: course_2.id)
+    StudentCourse.create!(student_id: student_3.id, course_id: course_3.id)
+    StudentCourse.create!(student_id: student_4.id, course_id: course_3.id)
+
+    visit '/courses'
+
+    expect(course_3.name).to appear_before(course_1.name)
+    expect(course_3.name).to appear_before(course_2.name)
+    expect(course_2.name).to appear_before(course_1.name)
+  end
+
 end

e15cc0c9982519dd5e204660445f931b71272018
Nikhil Shahi <shniks@gmail.com>
2022-08-10 10:55:22 -0700

    Created feature test for student show page.

---
+++ spec/features/students/user_can_view_courses_on_student_show_page_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'When I visit a student show page' do
+  it 'I see a list of student courses' do
+
+    student_1 = Student.create!(name: "Harry Potter", age: 15, house: "Slitherin")
+    student_2 = Student.create!(name: "Paulie Paul", age: 14, house: "Bangin")
+
+    course_1 = Course.create!(name: "Defense against the Dark Arts")
+    course_2 = Course.create!(name: "Herbology")
+    course_3 = Course.create!(name: "Potions")
+    course_4 = Course.create!(name: "Flying High")
+
+    StudentCourse.create!(student_id: student_1.id, course_id: course_1.id)
+    StudentCourse.create!(student_id: student_1.id, course_id: course_3.id)
+    StudentCourse.create!(student_id: student_1.id, course_id: course_4.id)
+    StudentCourse.create!(student_id: student_2.id, course_id: course_2.id)
+    StudentCourse.create!(student_id: student_2.id, course_id: course_3.id)
+
+    visit "/students/#{student_1.id}"
+
+    expect(page).to have_content("Name: Rando #{i}")
+    expect(page).to have_content("Age: #{i}")
+    expect(page).to have_content("House: House #{i}")
+  end
+end

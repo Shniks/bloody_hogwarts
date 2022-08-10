c59eff926daf64bd72e40f25c6c570c98248b433
Nikhil Shahi <shniks@gmail.com>
2022-08-10 11:13:48 -0700

    Created feature test to see all courses with the number of students on the courses index page.

---
+++ spec/features/courses/user_can_see_student_details_on_courses_index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'When I visit the courses index' do
+  it 'I see the number of students for each course' do
+
+    student_1 = Student.create!(name: "Harry Potter", age: 15, house: "Slitherin")
+    student_2 = Student.create!(name: "Paulie Paul", age: 14, house: "Bangin")
+    student_3 = Student.create!(name: "Frodo Baggins", age: 14, house: "Mordor")
+    student_4 = Student.create!(name: "Gandalf", age: 14, house: "Moria")
+
+    course_1 = Course.create!(name: "Defense against the Dark Arts")
+    course_2 = Course.create!(name: "Herbology")
+
+    StudentCourse.create!(student_id: student_1.id, course_id: course_1.id)
+    StudentCourse.create!(student_id: student_2.id, course_id: course_1.id)
+    StudentCourse.create!(student_id: student_4.id, course_id: course_1.id)
+    StudentCourse.create!(student_id: student_2.id, course_id: course_2.id)
+    StudentCourse.create!(student_id: student_3.id, course_id: course_2.id)
+
+    visit "/courses"
+
+    expect(page).to have_content("Defense against the Dark Arts": 3)
+    expect(page).to have_content("Herbology": 2)
+  end
+end

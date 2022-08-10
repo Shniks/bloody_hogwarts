4d23bd02d3c0258978b8ea9741c976adc75bd29c
Nikhil Shahi <shniks@gmail.com>
2022-08-10 12:05:46 -0700

    Created a test to see if students are in alphabetical order.

--- spec/features/students/user_can_see_details_of_student_spec.rb
+++ spec/features/students/user_can_see_details_of_student_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'When I visit students path' do
-  
+
   describe 'I see a list of students' do
     it 'I can see the name, age and house of each student' do
 
@@ -21,6 +21,7 @@ RSpec.describe 'When I visit students path' do
     end
 
     it 'I can see the average age of all students' do
+
       age = []
       (1..10).each do |i|
         Student.create!(name: "Rando #{i}", age: "#{rand(18..35)}", house: "House #{i}")
@@ -33,5 +34,18 @@ RSpec.describe 'When I visit students path' do
       expect(page).to have_content("Average Age: #{average_age}")
     end
   end
+
+    it 'The names of the students are listed in alphabetical order' do
+
+      student_1 = Student.create!(name: "Harry Potter", age: 15, house: "Slitherin")
+      student_2 = Student.create!(name: "Paulie Paul", age: 14, house: "Bangin")
+      student_3 = Student.create!(name: "Frodo Baggins", age: 19, house: "Mordor")
+
+      visit '/students'
+
+
+      expect(student_1.name).to appear_before(student_2.name)
+      expect(student_3.name).to appear_before(student_1.name)
+    end
 
 end

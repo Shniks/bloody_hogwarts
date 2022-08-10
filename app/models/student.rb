196791e493c300e762ca4e7356520ed49e811585
Nikhil Shahi <shniks@gmail.com>
2022-08-10 11:34:35 -0700

    Created average age method in student class. Updated students index view to show the average age.

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,5 +3,9 @@ class Student < ApplicationRecord
   has_many :courses, through: :student_courses
 
   validates_presence_of :name, :age, :house
-  
+
+  def self.average_age
+    average(:age)
+  end
+
 end
